8950e028f74c92515de4086aa3ef04014f46d929
michael <akrawimichael@gmail.com>
2018-07-02 12:09:53 +0300

    js draw function and winner/draw message

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -35,19 +35,43 @@ var game = {
             });
 
         });
+
+        let reset = $('#btn-reset');
+        reset.click(function () {
+            game.resetGame();
+        });
+
     },
 
     checkWinner: function (cell) {
 
-        if (game.checkVertical(cell) ||
-            game.checkHorizontal(cell) || 
-            game.checkSlant())
+        if (game.verticalWining(cell) ||
+            game.horizontalWining(cell) ||
+            game.slantWining())
 
-            window.alert('winner');
+            game.showWinnerMessage();
+        else if (game.checkDraw())
+            game.showDrawMessage();
+        
+           
 
     },
 
-    checkHorizontal: function ($cell) {
+    checkDraw: function () {
+
+        let draw = true;            
+        let $cells = $('td');
+
+        $cells.each(function () {
+            if ($(this).is(':empty'))
+                draw = false;                    
+
+        });
+
+        return draw;
+    },
+
+    horizontalWining: function ($cell) {
 
         let winner = true;
         const arr = [0, 1, 2];
@@ -57,7 +81,7 @@ var game = {
         let filter = arr.filter(x => x != index);
         for (let j = 0; j < filter.length; j++) {
             let c = row.children().eq(filter[j]);
-            if ($(c).attr('player') == undefined || $(c).attr('player') != game.turn) {
+            if (!game.hasAttr(c)) {
                 winner = false;
                 break;
             }
@@ -66,14 +90,14 @@ var game = {
         return winner;
     },
 
-    checkVertical: function (cell) {
+    verticalWining: function (cell) {
 
         const index = $(cell).index();
         const rows = $('tr');
         for (var j = 0; j < rows.length; j++) {
 
             const c = $(rows[j]).children().eq(index);
-            if ($(c).attr('player') == undefined || $(c).attr('player') != game.turn) {
+            if (!game.hasAttr(c)) {
                 return false;
             }
         }
@@ -81,47 +105,49 @@ var game = {
         return true;
     },
 
-    checkSlant: function () {
+    slantWining: function () {
 
-       return game.leftSlantWinner() || game.rightSlantWinner();
-            
+        return game.leftSlantWinner() || game.rightSlantWinner();
+
     },
 
-    leftSlantWinner : function(){
-            let winner = true;
-            const rows = $('tr');
-            for(let j = rows.length - 1 ;  j >= 0; j --){
-                let c   = $(rows[j]).children().eq(j);
-                if(!game.hasAtrr(c)){
-                        winner = false;
-                        break;
-                }
-    
-            }
-            
-            return winner;
-    
-    },
-
-    rightSlantWinner : function(){
-
+    leftSlantWinner: function () {
         let winner = true;
         const rows = $('tr');
-        for(let j = 0;  j < rows.length; j ++){
-            let c   = $(rows[j]).children().eq(j);
-            if(!game.hasAtrr(c)){
-                    winner = false;
-                    break;
+        let i = 2;
+        for (let j = 0; j < rows.length; j++) {
+            let c = $(rows[j]).children().eq(i);
+            if (!game.hasAttr(c)) {
+                winner = false;
+                break;
             }
-
+            i--;
         }
-        
+
         return winner;
 
     },
 
-    hasAtrr : function(cell){
-        return ($(cell).attr('player') == undefined || $(cell).attr('player') != game.turn);
+    rightSlantWinner: function () {
+
+        let winner = true;
+        const rows = $('tr');
+        for (let j = 0; j < rows.length; j++) {
+            let c = $(rows[j]).children().eq(j);
+            if (!game.hasAttr(c)) {
+                winner = false;
+                break;
+            }
+
+        }
+
+        return winner;
+
+    },
+
+    hasAttr: function (cell) {
+        let b = $(cell).attr('player') != undefined && $(cell).attr('player') == game.turn;
+        return b;
     },
 
     changePlayerTurn: function () {
@@ -144,6 +170,34 @@ var game = {
 
         cell.append($img);
         $(cell).attr('player', game.turn);
+    },
+
+    showWinnerMessage: function () {
+
+        let message = document.querySelector('#message');
+        message.innerHTML = `The Winner Is ${game.turn}`;
+        $('#winner').modal('show');
+
+    },
+
+    showDrawMessage: function () {
+
+        let message = document.querySelector('#message');
+        message.innerHTML = `A Draw! Try Agian`;
+        $('#winner').modal('show');
+    },
+
+    resetGame: function () {
+
+        let cells = document.querySelectorAll('td');
+        cells.forEach(function (c) {
+            $(c).empty();
+            $(c).removeAttr('player');
+        });
+
+        game.turn = 'player1';
+        $('#winner').modal('hide');
+
     }
 
 };
